b88b6b1b50176b8bc39d336adcf3e96bedc545c3
pferri <pau.ferri@fasanara.com>
2023-02-21 18:50:14 +0000

    added credentials class

---
+++ credentials.py
@@ -0,0 +1,24 @@
+import configparser
+from pathlib import Path
+
+
+class Credentials:
+    """
+    Credentials class to read the credentials for different of accounts, emails, apis or databases
+    This information is usually stored locally and gitignored in the repo under the name 'config.ini'
+    """
+    # the parents of the ROOT DIR depend on the class Credentials is defined.
+    ROOT_DIR = Path(__file__).parents[0]
+    # the same for the .joinpath(), depending on where the 'config.ini' is placed.
+    CONFIG_FILENAME = ROOT_DIR.joinpath('config.ini')
+
+    def __init__(self):
+        self.config = configparser.ConfigParser()
+        self.config.read(self.CONFIG_FILENAME)
+
+    def get_elexon_api_key(self):
+        definition = 'ELEXON_CREDENTIALS'
+        credential_dict = {
+            'api_key': self.config[definition]['api_key']
+        }
+        setattr(self, definition, credential_dict)
