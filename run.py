4a2d5131440629004cadca565db68c4d8bba2fb2
pferri <pau.ferri@fasanara.com>
2023-02-22 15:09:25 +0000

    completed the run file

--- run.py
+++ run.py
@@ -1,22 +1,23 @@
 from src.api_elexon_caller import ElexonCaller
 from src.helper import adding_minutes_to_df
-from src.plotter import Plotter
 from src.metrics import Report
-
+import datetime as dt
 
 if __name__ == '__main__':
 
-    eep = ElexonCaller(settlement_date='2015-03-01')
+    today = dt.datetime.today()
+    yesterday = today - dt.timedelta(days=1)
 
-    daily_imbalanced_prices = eep.get_daily_imbalanced_prices()
-    daily_aggregated_imbalanced_volumes = eep.get_daily_aggregated_imablance_volumes()
+    ec = ElexonCaller(settlement_date=yesterday.strftime('%Y-%m-%d'))
+
+    daily_imbalanced_prices = ec.get_daily_imbalanced_prices()
+    daily_aggregated_imbalanced_volumes = ec.get_daily_aggregated_imablance_volumes()
 
     adding_minutes_to_df(daily_imbalanced_prices)
     adding_minutes_to_df(daily_aggregated_imbalanced_volumes)
 
-    Report(daily_imbalanced_prices, daily_aggregated_imbalanced_volumes)
-
-    plotter = Plotter()
-    plotter.plot_imbalance_prices(daily_imbalanced_prices)
-    plotter.plot_imbalance_quantity(daily_aggregated_imbalanced_volumes)
+    daily_report = Report(daily_imbalanced_prices, daily_aggregated_imbalanced_volumes)
+    daily_report.print_simple_description()
+    daily_report.print_simple_statistics()
+    daily_report.plot_graphs()
 
