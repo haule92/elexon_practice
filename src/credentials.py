a7f01c51530a75d50d861ea5cfe5f77cd1598b99
pferri <pau.ferri@fasanara.com>
2023-02-22 15:11:37 +0000

    amended documentation for the credentials class

--- src/credentials.py
+++ src/credentials.py
@@ -3,13 +3,11 @@ from pathlib import Path
 
 
 class Credentials:
-    """
-    Credentials class to read the credentials for different of accounts, emails, apis or databases
-    This information is usually stored locally and gitignored in the repo under the name 'config.ini'
-    """
-    # the parents of the ROOT DIR depend on the class Credentials is defined.
+    """Credentials class to read the credentials for different of accounts, emails, apis or databases.
+    This information is usually stored locally and gitignored in the repo under the name 'config.ini'.
+    The parents of the ROOT DIR depends on where the class Credentials is defined."""
+
     ROOT_DIR = Path(__file__).parents[1]
-    # the same for the .joinpath(), depending on where the 'config.ini' is placed.
     CONFIG_FILENAME = ROOT_DIR.joinpath('config.ini')
 
     def __init__(self):
