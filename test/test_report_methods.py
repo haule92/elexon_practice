92ac0c65c43f66721cdd43ca36f695d9464f586f
pferri <pau.ferri@fasanara.com>
2023-02-22 15:08:40 +0000

    documentation written plus added another test

--- test/test_report_methods.py
+++ test/test_report_methods.py
@@ -3,6 +3,9 @@ from src.metrics import Report
 
 
 def create_test_imbalance_price_data():
+    """Creation of dummy data to test the correctness of the methods from the report class.
+    The method returns a dataframe simulating the imbalance prices data"""
+
     col1 = ['Insufficient balance', 'Excess balance', 'Excess balance', 'Insufficient balance', 'Excess balance']
     col2 = [34.65, 761.23, 86.12, 12.98, 78.3333]
     data = {'priceCategory': col1, 'imbalancePriceAmountGBP': col2}
@@ -11,6 +14,9 @@ def create_test_imbalance_price_data():
 
 
 def create_test_aggregated_volume_data():
+    """Creation of dummy data to test the correctness of the methods from the report class.
+    The method returns a dataframe simulating the aggregated volume data"""
+
     col1 = ['col1', 'col1', 'col1', 'col1', 'col1']
     col2 = [932.12, 617.58, 905.54, 176.8, 381.9999]
     data = {'col1': col1, 'imbalanceQuantityMAW': col2}
@@ -54,3 +60,10 @@ def test_compute_highest_absolut_volume():
     result = Report(imbalance_price, aggregated_volume).compute_highest_absolut_volume()
 
     assert result == '932.12'
+
+
+def test_statistics():
+    imbalance_price = create_test_imbalance_price_data()
+    aggregated_volume = create_test_aggregated_volume_data()
+    result = Report(imbalance_price, aggregated_volume).compute_basic_statistics_aggregated_volumne()
+    assert type(result) is dict
